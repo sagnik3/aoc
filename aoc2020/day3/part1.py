7b0f554ff95faa9e4ed5dbfa5207d72e2f112633
Sagnik Chatterjee <chatterjeesagnik33@gmail.com>
2022-06-24 19:48:50 +0530

    Solved part1.py

--- aoc2020/day3/part1.py
+++ aoc2020/day3/part1.py
@@ -1,28 +1,26 @@
+
+def part1(s:str) -> int:
+    lines = s.splitlines()
+    x,y=0,0
+
+    count = 0
+    x +=3 
+    x %= len(lines[0])
+    y +=1
+    while y< len(lines):
+        if lines[y][x] =="#":
+            count+=1
+        x +=3
+        x %= len(lines[0])
+        y +=1
+    return count 
+
 def main():
-    with open('input.txt','r') as f:
-        p= f.readlines()
-
-    p=[x.strip() for x in p]
-    p = [ line[0:-1] for line in p]
-    
-    width =  len(p[0])
-
-    def check(right,bottom):
-        x, y = 0 , 0
-        count = 0
-
-        while not  y >= len(p) - 1:
-            x += right
-            y += bottom 
-
-            if x>= width:
-                x -= width 
-            if p[y][x] == "#":
-                count +=1
-        return count 
+    with open('input.txt') as f:
+        print(part1(f.read()))
 
 
-    print(check(3,1))
+if __name__=='__main__':
+    main()
 
 
-main()
